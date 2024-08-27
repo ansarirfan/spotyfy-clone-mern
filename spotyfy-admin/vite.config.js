fa9fe2474aa32ac242fd4f3ed4e8d2fb45a1f17e
Irfan Ansari <103310054+ansarirfan@users.noreply.github.com>
2024-08-27 10:01:08 +0530

    Update vite.config.js

--- spotyfy-admin/vite.config.js
+++ spotyfy-admin/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Customize the output directory here
+    outDir: 'dist', // Replace with your desired output directory, e.g., './build', 'dist', or 'frontend/build'
+  },
 })
